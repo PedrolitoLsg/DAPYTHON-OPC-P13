bdf29bbbb66ce2c37457e99f8709acadebd2ec01
PedrolitoLsg <84906663+PedrolitoLsg@users.noreply.github.com>
2022-05-10 14:28:42 +0200

    modularizing

---
+++ lettings/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import letting, index
+
+
+
+urlpatterns = [
+    path('lettings/', index, name='lettings_index'),
+    path('lettings/<int:letting_id>/', letting, name='letting')
+]
