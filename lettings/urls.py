e981022a46a9da279b1adb82d2e87868be1e4e70
PedrolitoLsg <84906663+PedrolitoLsg@users.noreply.github.com>
2022-05-13 14:14:09 +0200

    dbchange

--- lettings/urls.py
+++ lettings/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import letting, index
 
 
-
 urlpatterns = [
     path('lettings/', index, name='lettings_index'),
     path('lettings/<int:letting_id>/', letting, name='letting')
--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -62,11 +62,22 @@ WSGI_APPLICATION = 'oc_lettings_site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'oc-lettings-site.sqlite3'),
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db13',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
