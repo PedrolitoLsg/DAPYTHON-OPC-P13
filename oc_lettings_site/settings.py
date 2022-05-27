cf9bfa44657cd11bcf350bc357ca8955e0437943
PedrolitoLsg <84906663+PedrolitoLsg@users.noreply.github.com>
2022-05-27 11:50:40 +0200

    dbbackswitch

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = 'oc_lettings_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -81,7 +81,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
