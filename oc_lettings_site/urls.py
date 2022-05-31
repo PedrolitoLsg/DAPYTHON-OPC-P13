4cb588dcc113f43122059977669ba8117c6e411f
PedrolitoLsg <84906663+PedrolitoLsg@users.noreply.github.com>
2022-05-31 18:30:38 +0200

    sentrydebug

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from . import views
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('lettings/', include('lettings.urls')),
-    path('profiles/', include('profiles.urls'))
+    path('profiles/', include('profiles.urls')),
+    path('sentry-debug/', trigger_error()),
 ]
+
