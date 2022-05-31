27db4736f50fa58a351cfb3c081d4c2bc29d31d8
PedrolitoLsg <84906663+PedrolitoLsg@users.noreply.github.com>
2022-05-31 18:39:58 +0200

    imprequest

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.shortcuts import request
+
 
 def trigger_error(request):
     division_by_zero = 1 / 0
